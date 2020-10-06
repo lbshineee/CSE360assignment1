72ad590c43edc8f13d70ae2f19269e0a55acabed
Leo J <ar3021113497@gmail.com>
2020-10-05 22:24:47 -0700

    Initial change of the class.

---
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,38 @@
+package cse360assignment02;
+
+public class AddingMachine 
+{
+	//declear class variables
+	private int total;
+	//Constructor
+	public AddingMachine()
+	{
+		total = 0;	//not needed, but still I put it here brabrabra..
+	}
+	//Getter method to get total amount. Return an integer.
+	public int getTotal()
+	{
+		return 0;
+	}
+	//A method to add. It takes an integer.
+	public void add(int value)
+	{
+		
+	}
+	//A method to subtract. It takes an integer.
+	public void subtract(int value)
+	{
+		
+	}
+	//toString method to creating a string. It returns a string.
+	public String toString()
+	{
+		return " "+"new change12";
+	}
+	//a method to clear. 
+	public void clear()
+	{
+		
+	} 
+
+}
